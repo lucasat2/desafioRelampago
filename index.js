df727a5dcd78799d21059da54ca858610ab23f4d
wanessaneves <wanessanevsm@gmail.com>
2025-01-28 10:06:10 -0300

    modelo inicial

---
+++ index.js
@@ -0,0 +1,32 @@
+const campoEntradas = document.getElementById('entradas');
+const botaoSortear = document.getElementById('botaoSortear');
+const botaoResetar = document.getElementById('botaoResetar');
+const exibicaoVencedor = document.getElementById('vencedor');
+const campoParticipantes = document.getElementById('participantes');
+const campoSorteios = document.getElementById('sorteios');
+const exibicaoProbabilidade = document.getElementById('probabilidade');
+const secaoHistorico = document.getElementById('historico');
+const secaoSorteados = document.getElementById('sorteados');
+
+function calcularProbabilidade(participantes, sorteios) {
+    if (participantes <= 0 || sorteios <= 0) return 0;
+    return (1 - Math.pow((participantes - 1) / participantes, sorteios)) * 100;
+}
+
+function atualizarProbabilidade() {
+    const participantes = parseInt(campoParticipantes.value, 10);
+    const sorteios = parseInt(campoSorteios.value, 10);
+
+    const probabilidade = calcularProbabilidade(participantes, sorteios);
+    exibicaoProbabilidade.textContent = 
+        `A probabilidade de ganhar é ${probabilidade.toFixed(2)}%`;
+}
+
+
+function atualizarParticipantes() {
+    const regexNomeValido = /^[a-zA-Zá-úÁ-ÚãõÃÕêÊéÉíÍóÓúÚçÇ\s]+$/;
+    const entradas = campoEntradas.value.split(',').map(e => e.trim()).filter(e => regexNomeValido.test(e));
+    campoParticipantes.value = entradas.length;
+
+    atualizarProbabilidade();
+}
